b8e91e398db2c50b81386997055f5ab734055f78
StrikeHERE29 <vasilecristian2901@yahoo.com>
2024-03-24 13:40:30 +0200

    added functionality to update the remaining time

--- countdown.js
+++ countdown.js
@@ -29,7 +29,7 @@ let giveaway = document.querySelector(".giveaway");
 let items = document.querySelectorAll(".btn h2");
 
 
-let futureDate = new Date(2024, 4, 20, 14, 30, 0);
+let futureDate = new Date(2024, 4, 20, 15, 30, 0);
 console.log(futureDate);
 
 const year = futureDate.getFullYear();
@@ -48,3 +48,49 @@ let weekDay = futureDate.getDay();
 weekDay = weekDays[weekDay];
 
 giveaway.textContent = `giveaway ends on ${weekDay} ${date} ${month} ${year} ${hours}:${minutes}pm`;
+
+//future time in ms
+const futureTime = futureDate.getTime();
+
+
+function getRemainingTime(){
+    const today = new Date().getTime();
+    const t = futureTime - today;
+    //console.log(t);
+    // 1s = 1000ms
+    // 1m = 60s 
+    // 1hr = 60min
+    // 1d = 24hr
+
+    // values in ms
+
+    const oneDay = 24*60*60*1000;
+    const oneHour = 60*60*1000;
+    const oneMinute = 60*1000;
+
+    //calculate all values
+    let days = Math.floor(t/oneDay);
+    //console.log(days);
+    let hours = Math.floor((t%oneDay)/oneHour);
+    //console.log(hours)
+    let minutes = Math.floor((t%oneHour)/oneMinute);
+    //console.log(minutes);
+    let seconds = Math.floor((t%oneMinute)/ 1000);
+    //console.log(seconds)
+
+    // set values array
+    const values = [days,hours,minutes,seconds];
+
+    function format(item){
+        if(item < 10){
+            return item =`0${item}`
+        }
+        return item
+    }
+
+    items.forEach(function(item,index){
+        item.innerHTML = format(values[index]);
+    });
+}
+
+getRemainingTime();
