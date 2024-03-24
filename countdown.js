4fa3399fe9043c35236e96b7310e13fafc1c8e85
StrikeHERE29 <vasilecristian2901@yahoo.com>
2024-03-24 13:11:18 +0200

    added variables for the date and updated the subtitle

--- countdown.js
+++ countdown.js
@@ -14,13 +14,14 @@ let months = [
 ]
 
 let weekDays = [
+   "Sunday",
     "Monday",
     "Tuesday",
     "Wednesday",
     "Thursday",
     "Friday",
     "Saturday",
-    "Sunday"
+    
 ]
 
 let deadline = document.querySelector(".deadline");
@@ -28,3 +29,22 @@ let giveaway = document.querySelector(".giveaway");
 let items = document.querySelectorAll(".btn h2");
 
 
+let futureDate = new Date(2024, 4, 20, 14, 30, 0);
+console.log(futureDate);
+
+const year = futureDate.getFullYear();
+//console.log(year)
+const hours = futureDate.getHours();
+//console.log(hours);
+const minutes = futureDate.getMinutes();
+//console.log(minutes)
+const date = futureDate.getDate();
+//console.log(date);
+
+let month = futureDate.getMonth();
+month = months[month];
+
+let weekDay = futureDate.getDay();
+weekDay = weekDays[weekDay];
+
+giveaway.textContent = `giveaway ends on ${weekDay} ${date} ${month} ${year} ${hours}:${minutes}pm`;
